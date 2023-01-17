3e7f4f75570e9b4c80f2bad44681d79be3261862
rcmanaure <rcmanaure@gmail.com>
2023-01-17 07:29:43 -0400

    scrapper-03 added new columns to CSV file2

--- scrapper_beau.py
+++ scrapper_beau.py
@@ -22,29 +22,6 @@ my_headers = {
 session = requests.Session()
 pages = 1
 soup_list = []
-# with requests.Session() as s:
-
-#     while pages < 3:
-#         sleep(10)
-#         url = f"https://www.infojobs.net/jobsearch/search-results/list.xhtml?keyword=Programador%2Fa%20web&normalizedJobTitleIds=2512_866c7813-2c03-47d7-9bdc-192cfbace57c&provinceIds=&cityIds=&teleworkingIds=&categoryIds=&workdayIds=&educationIds=&segmentId=&contractTypeIds=&page={pages}&sortBy=PUBLICATION_DATE&onlyForeignCountry=false&countryIds=&sinceDate=ANY&subcategoryIds="
-
-#         response = s.get(url, headers=my_headers)
-
-#         soup = BeautifulSoup(response.text, "html.parser")
-#         soup_list.append(soup)
-#         pages += 1
-
-# print(soup_list[0])
-# with open("infojobs_html.json", "wb") as fp:
-#     # To write data to new file
-#     json.dump(soup_list, fp)
-
-
-with open("infojobs_html.txt", "r") as fh:
-
-    html_soup = BeautifulSoup(fh, "html.parser")
-
-
 company_names = []
 company_urls = []
 company_roles = []
@@ -55,6 +32,31 @@ contract_types = []
 work_schedules = []
 salaries = []
 cities = []
+with requests.Session() as s:
+
+    while pages < 2:
+        sleep(10)
+        url = f"https://www.infojobs.net/jobsearch/search-results/list.xhtml?keyword=Programador%2Fa%20web&normalizedJobTitleIds=2512_866c7813-2c03-47d7-9bdc-192cfbace57c&provinceIds=&cityIds=&teleworkingIds=&categoryIds=&workdayIds=&educationIds=&segmentId=&contractTypeIds=&page={pages}&sortBy=PUBLICATION_DATE&onlyForeignCountry=false&countryIds=&sinceDate=ANY&subcategoryIds="
+
+        response = s.get(url, headers=my_headers)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "html.parser")
+        soup_list.append(soup)
+        pages += 1
+
+# print(soup_list[0])
+# with open("infojobs_html.json", "wb") as fp:
+#     # To write data to new file
+#     json.dump(soup_list, fp)
+
+# with open("infojobs_html_3.text", "w") as fp:
+#     # To write data to new file
+#     fp.write(response.text)
+
+# with open("infojobs_html.txt", "r") as fh:
+
+#     html_soup = BeautifulSoup(fh, "html.parser")
+
 
 # for i in soup_list:
 #     # test = BeautifulSoup(i, "lxml")
@@ -105,86 +107,86 @@ cities = []
 #     ],
 # )
 
-container = html_soup.find_all(
-    ["div"],
-    class_=["ij-OfferCardContent-description"],
-)
+# container = html_soup.find_all(
+#     ["div"],
+#     class_=["ij-OfferCardContent-description"],
+# )
 
-# container_list = []
-for lines in container:
-    if lines.name == "div":
-        container_2 = lines.find_all(
-            ["h2", "h3", "li"],
-            class_=[
-                "ij-OfferCardContent-description-title",
-                "ij-OfferCardContent-description-subtitle",
-                "ij-OfferCardContent-description-list-item",
-            ],
-        )
+# # container_list = []
+# for lines in container:
+#     if lines.name == "div":
+#         container_2 = lines.find_all(
+#             ["h2", "h3", "li"],
+#             class_=[
+#                 "ij-OfferCardContent-description-title",
+#                 "ij-OfferCardContent-description-subtitle",
+#                 "ij-OfferCardContent-description-list-item",
+#             ],
+#         )
 
-        # Tecnologia
-        h2_roles = container_2[0].text
-        company_roles.append(h2_roles)
-        # URL Vacante
-        url_h2 = lines.find_all("a", href=re.compile("//www.infojobs.net/"))[0].get(
-            "href"
-        )
-        format_url2 = f"https:{url_h2}"
-        roles_urls.append(format_url2)
-        # Nombre de empresa
-        h3 = container_2[1].text
-        company_names.append(h3)
-        # URL Empresa (InfoJobs)
-        url_h3 = lines.find_all(
-            "a", class_="ij-OfferCardContent-description-subtitle-link"
-        )[0].get("href")
-        company_urls.append(url_h3)
+#         # Tecnologia
+#         h2_roles = container_2[0].text
+#         company_roles.append(h2_roles)
+#         # URL Vacante
+#         url_h2 = lines.find_all("a", href=re.compile("//www.infojobs.net/"))[0].get(
+#             "href"
+#         )
+#         format_url2 = f"https:{url_h2}"
+#         roles_urls.append(format_url2)
+#         # Nombre de empresa
+#         h3 = container_2[1].text
+#         company_names.append(h3)
+#         # URL Empresa (InfoJobs)
+#         url_h3 = lines.find_all(
+#             "a", class_="ij-OfferCardContent-description-subtitle-link"
+#         )[0].get("href")
+#         company_urls.append(url_h3)
 
-        # Ciudad
-        li_0 = container_2[2].text
-        cities.append(li_0)
-        # Tipo de trabajo
-        li_1 = container_2[3].text
-        work_types.append(li_1)
-        # Fecha de creación de Vacante
-        li_2 = container_2[4].text
-        vacancy_dates.append(li_2)
-        # Tipo de contrato
-        li_3 = container_2[5].text
-        contract_types.append(li_3)
-        # Horario de trabajo
-        li_6 = container_2[6].text
-        work_schedules.append(li_6)
-        # Salario
-        li_7 = container_2[7].text
-        salaries.append(li_7)
+#         # Ciudad
+#         li_0 = container_2[2].text
+#         cities.append(li_0)
+#         # Tipo de trabajo
+#         li_1 = container_2[3].text
+#         work_types.append(li_1)
+#         # Fecha de creación de Vacante
+#         li_2 = container_2[4].text
+#         vacancy_dates.append(li_2)
+#         # Tipo de contrato
+#         li_3 = container_2[5].text
+#         contract_types.append(li_3)
+#         # Horario de trabajo
+#         li_6 = container_2[6].text
+#         work_schedules.append(li_6)
+#         # Salario
+#         li_7 = container_2[7].text
+#         salaries.append(li_7)
 
-# Creating the CSV file
-df = pd.DataFrame(
-    {
-        "Nombre de empresa": company_names,
-        "Tecnologia": company_roles,
-        "Ciudad": cities,
-        "Fecha de creación de Vacante": vacancy_dates,
-        "Type of work": work_types,
-        "URL Empresa": company_urls,
-        "URL Vacante": roles_urls,
-        "Tipo de contratacion": contract_types,
-        "Horario de trabajo": work_schedules,
-        "Salario": salaries,
-    }
-)
+# # Creating the CSV file
+# df = pd.DataFrame(
+#     {
+#         "Nombre de empresa": company_names,
+#         "Tecnologia": company_roles,
+#         "Ciudad": cities,
+#         "Fecha de creación de Vacante": vacancy_dates,
+#         "Type of work": work_types,
+#         "URL Empresa": company_urls,
+#         "URL Vacante": roles_urls,
+#         "Tipo de contratacion": contract_types,
+#         "Horario de trabajo": work_schedules,
+#         "Salario": salaries,
+#     }
+# )
 
-cols = [
-    "Nombre de empresa",
-    "Tecnologia",
-    "Ciudad",
-    "Fecha de creación de Vacante",
-    "Type of work",
-    "URL Empresa",
-    "URL Vacante",
-    "Tipo de contratacion",
-    "Horario de trabajo",
-    "Salario",
-]
-df.to_csv("data/infojobs-1.csv", encoding="utf-8", index=False, columns=cols)
+# cols = [
+#     "Nombre de empresa",
+#     "Tecnologia",
+#     "Ciudad",
+#     "Fecha de creación de Vacante",
+#     "Type of work",
+#     "URL Empresa",
+#     "URL Vacante",
+#     "Tipo de contratacion",
+#     "Horario de trabajo",
+#     "Salario",
+# ]
+# df.to_csv("data/infojobs-demo.csv", encoding="utf-8", index=False, columns=cols)
